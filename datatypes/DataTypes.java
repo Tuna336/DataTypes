d71b0f2c2ca14ce8c4c668658e6ce6343ce3ae24
Tuna w/ Black Airforces <143442984+Tuna336@users.noreply.github.com>
2023-09-12 11:43:41 -0700

    Add files via upload

---
+++ datatypes/DataTypes.java
@@ -0,0 +1,10 @@
+    public class DataTypes
+    {
+        public static void main(String[] args)
+        {
+            int i;
+            double f = 2.5;
+            boolean flag = true;
+            System.out.println(f);
+        }
+    }
